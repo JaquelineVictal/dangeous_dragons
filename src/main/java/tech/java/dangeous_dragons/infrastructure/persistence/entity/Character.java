16f122dbd690d7a5c527be43d9f22706816e23cd
Jaqueline Victal <jaquelinevictal@gamil.com>
2023-09-09 15:59:23 -0300

    feat: creates the Character entity

---
+++ src/main/java/tech/java/dangeous_dragons/infrastructure/persistence/entity/Character.java
@@ -0,0 +1,21 @@
+package tech.java.dangeous_dragons.infrastructure.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "character")
+public class Character {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "character_type", referencedColumnName = "character_type")
+    private CharacterConfig characterConfig;
+}
