2e0463e054a70a296372dbb0b9b334803dae0532
Jaqueline Victal <jaquelinevictal@gamil.com>
2023-09-09 12:38:18 -0300

    init project

---
+++ src/main/java/tech/java/dangeous_dragons/DangeousDragonsApplication.java
@@ -0,0 +1,13 @@
+package tech.java.dangeous_dragons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangeousDragonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangeousDragonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/java/dangeous_dragons/DangeousDragonsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.java.dangeous_dragons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangeousDragonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
