9ddd9e51694a95c0a1a1da7d927d256a17d69634
Jaqueline Victal <jaquelinevictal@gamil.com>
2023-09-12 23:58:17 -0300

    feat: do not return Character with soft delete

--- src/main/java/tech/java/dangeous_dragons/domain/service/character/GetByIdCharacterService.java
+++ src/main/java/tech/java/dangeous_dragons/domain/service/character/GetByIdCharacterService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import tech.java.dangeous_dragons.application.mapper.CharacterToCreateCharacterResponseMapper;
 import tech.java.dangeous_dragons.application.payloads.response.character.CharacterResponse;
+import tech.java.dangeous_dragons.common.exceptions.NotFoundException;
 import tech.java.dangeous_dragons.infrastructure.persistence.entity.character.Character;
 import tech.java.dangeous_dragons.infrastructure.persistence.repository.character.CharacterRepository;
 
@@ -15,7 +16,8 @@ public class GetByIdCharacterService {
     private final CharacterRepository characterRepository;
 
     public CharacterResponse execute(Long id) {
-        Character character = characterRepository.getReferenceById(id);
+        Character character = characterRepository.findByIdAndDeletedAtIsNull(id)
+                .orElseThrow(() -> new NotFoundException("Character"));
         return CharacterToCreateCharacterResponseMapper.execute(character);
     }
 }
